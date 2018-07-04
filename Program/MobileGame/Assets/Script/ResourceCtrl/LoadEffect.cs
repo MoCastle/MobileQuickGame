4cba13fd9c4a89b07a8373a3135d0344f41f3ba4
MoCastle <email@example.com>
2018-07-04 18:32:29 +0800

    导入插件前备份

---
+++ Program/MobileGame/Assets/Script/ResourceCtrl/LoadEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadEffect : MonoBehaviour {
+    Dictionary<string, GameObject> ObjDict = new Dictionary<string, GameObject>( );
+	// Use this for initialization
+	public GameObject LoadEffectObj( string Name )
+    {
+        GameObject ReturnObj = new GameObject();
+        if( !ObjDict.TryGetValue( Name,out ReturnObj) )
+        {
+            string Path = "Prefab/Effect" + Name;
+            ReturnObj = Resources.Load(Path,typeof( GameObject )) as GameObject;
+            ObjDict.Add(Name, ReturnObj);
+        }
+        return ReturnObj;
+    }
+}

9544bfbdc67cd7dbcdf802cfc53c060b60e21ab4
LithMo <18224837254@163.com>
2018-08-18 22:15:36 +0800

    增加自动生成配置表代码功能(进行中)

---
+++ AutoCfgCodeMaker/CfgCodeMaker/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CfgCodeMaker
+{
+    class Program
+    {
+        //获取配置表文件夹路径
+        static string ReadFiel = "\\CfgFiel";
+        static string GetReadPath
+        {
+            get
+            {
+                string ReturnPath = Directory.GetCurrentDirectory() + ReadFiel;
+
+                if (!Directory.Exists(ReturnPath))
+                {
+                    Directory.CreateDirectory(ReturnPath);
+                }
+                return ReturnPath;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //读取路径下的所有文件
+            DirectoryInfo root = new DirectoryInfo(GetReadPath);
+            FileInfo[] files = root.GetFiles();
+            foreach( FileInfo File in files )
+            {
+                Console.WriteLine(File.FullName);
+            }
+            Console.ReadKey();
+        }
+    }
+}

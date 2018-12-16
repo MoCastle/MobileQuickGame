5ae1c8fa9a2586ca87eaf99cb1ecf4648268d2b5
MoCastle <38985297+MoCastle@users.noreply.github.com>
2018-12-16 22:44:36 +0800

    调整更新

--- Program/MobileGame/Assets/Scence/TestScript.cs
+++ Program/MobileGame/Assets/Scence/TestScript.cs
@@ -1,13 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
 
 public class TestScript : MonoBehaviour {
-
+    public Text text;
 	// Use this for initialization
 	void Start () {
-        GetComponent<Animator>().speed = 0;
-        //Debug.Log(text.text);
+
+        string Path = Application.dataPath+"\n" + Application.streamingAssetsPath;
+
+        StreamReader Reader = File.OpenText("jar:file://" + Application.dataPath + "!/assets/" + "ActorInfo.csv");
+
+            //new StreamReader( Path,)//(Application.dataPath+ "/ActorInfo.csv", true, Encoding.UTF8);
+        text.text = Reader.ReadToEnd();
+        Reader.Close();
+        Reader.Dispose();
+        //text.text += writer.read
     }
 	
 	// Update is called once per frame

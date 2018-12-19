fce3ee8b58c9a5bd59666e21ba2a807a9897366c
MoCastle <38985297+MoCastle@users.noreply.github.com>
2018-12-19 22:13:14 +0800

    准备场景切换功能

--- Program/MobileGame/Assets/Script/Base/GameControl/GameDriver.cs
+++ Program/MobileGame/Assets/Script/Base/GameControl/GameDriver.cs
@@ -6,42 +6,19 @@ using PathologicalGames;
 //游戏总控制器 负责游戏核心逻辑
 public class GameDriver : MonoBehaviour
 {
-
-    public GameObject PlayerSample;
     private void Awake()
     {
-        /*
-        try
-        {
-            CfgMgr.InitCfg();
-        }
-        catch
-        {
 
-        }
-
-        LogMgr.InitSet();
-
-        //UI用
-        WindowMgr.UIWindow = transform.FindChild("UICanvas");
-        GameCtrler.PlayerSample = PlayerSample;
-        */
         GameCtrl GameCtrler = GameCtrl.GameCtrler;
         GameCtrler.Pool = GetComponent<SpawnPool>();
 
         DontDestroyOnLoad(this.gameObject);
-        //GameWorldTimer.StartGameSet();
-
-        
     }
     private void Start()
     {
-        //Application.LoadLevel("EnterSence");
     }
     private void Update()
     {
-        //GameWorldTimer.Update();
         LogMgr.Update();
-        //GameCtrl.GameCtrler.Update();
     }
 }

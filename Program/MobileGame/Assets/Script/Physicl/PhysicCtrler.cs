5019ba42745e04edbf5a3bb6c7911478e43c4b30
lijiang110 <40169020+lijiang110@users.noreply.github.com>
2019-03-30 23:45:29 +0800

    村庄地形更新

--- Program/MobileGame/Assets/Script/Physicl/PhysicCtrler.cs
+++ Program/MobileGame/Assets/Script/Physicl/PhysicCtrler.cs
@@ -85,11 +85,11 @@ public class PhysicCtrler {
         {
             return;
         }
-        if (!_LowGravity && Mathf.Abs(_Rigid2D.velocity.y) < 2)
+        if (!_LowGravity && Mathf.Abs(_Rigid2D.velocity.y) < 3)
         {
             SetGravity(OrGravity / 10);
         }
-        else if (_LowGravity&& Mathf.Abs(_Rigid2D.velocity.y) > 2)
+        else if (_LowGravity&& Mathf.Abs(_Rigid2D.velocity.y) > 3)
         {
             ResetGravity();
         }

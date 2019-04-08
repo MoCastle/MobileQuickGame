770610dbc5ce23a7d9dc20b30f012d03ee5edccf
MoCastle <38985297+MoCastle@users.noreply.github.com>
2019-04-08 21:59:07 +0800

    技能处理

---
+++ Program/MobileGame/Assets/Script/Animator/FlySkillAnimAdapt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+using System;
+using GameScene;
+
+public class FlySkillAnimAdapt : AnimAdapt
+{
+    [Header("运动曲线")]
+    public AnimationCurve Curve;
+    [Header("运动速度")]
+    public float MoveSpeed;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (ActionName == null || ActionName == "")
+        {
+            ActionName = "BaseAction";
+        }
+        BaseActorObj NoticeObject = animator.gameObject.GetComponentInParent<BaseActorObj>();
+        Assembly assembly = Assembly.GetExecutingAssembly(); // 获取当前程序集 
+        Type GetState = assembly.GetType(ActionName);
+        BaseAction NewState = (BaseAction)Activator.CreateInstance(GetState, new object[] { NoticeObject, SkillInfo }); // 创建类的实例，返回为 object 类型，需要强制类型转换
+        NoticeObject.ActionCtrl.CurAction = NewState;
+        NoticeObject.SwitchAction();
+    }
+}
---
+++ Program/MobileGame/Assets/Script/Base/FSM/FSM.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace BaseFunc
+{
+    public class BaseFSM
+    {
+        #region 内部属性
+        BaseState m_CurState;
+        #endregion
+        #region 流程
+        public BaseFSM()
+        {
+
+        }
+
+        public virtual void Switch( BaseState state )
+        {
+            if(m_CurState!=null)
+            {
+                m_CurState.End();
+            }
+            state.SetOwner(this);
+            m_CurState = state;
+        }
+        #endregion
+    }
+
+    public abstract class BaseState
+    {
+        #region 内部属性
+        BaseFSM m_Owner;
+        #endregion
+        #region 流程
+        public BaseState()
+        {
+        }
+
+        public abstract void Start();
+        public abstract void End();
+        public abstract void Update();
+
+        public void SetOwner( BaseFSM owner )
+        {
+            m_Owner = owner;
+        }
+        #endregion
+    }
+}
